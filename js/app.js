dbc9a3047b4197b297b78e28bba9f8ced03daf08
Miillla <p.milana0510@gmail.com>
2024-08-04 17:40:45 +0300

    second commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 let firstName = "John";
 let lastName = "Doe";
 let age = 35;
-console.log(firstName, lastName, age);
+let job = "Web Developer";
+
+let fullName = `Fullname: ${firstName} ${lastName} ${age} ${job}`;
+
+console.log(firstName, lastName, age,job);
 console.log(typeof firstName, typeof lastName, typeof age);
