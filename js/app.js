bfaac21d73afdad5fafd89e7bfdf62b5e5b6c5ec
Miillla <p.milana0510@gmail.com>
2024-08-05 15:54:31 +0300

    fixed index and js files

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
-let firstName = "John";
-let lastName = "Doe";
-let age = 35;
-let job = "Web Developer";
+// let firstName = "John";
+// let lastName = "Doe";
+// let age = 35;
+// let job = "Web Developer";
+
+let firstName = prompt("Enter your first name:");
+let lastName = prompt("Enter your last name:");
+let age = prompt("Enter your age:");
+let job = prompt("Enter your job:");
 
 let fullName = `Fullname: ${firstName} ${lastName} ${age} ${job}`;
+console.log(fullName);
 
-console.log(firstName, lastName, age,job);
-console.log(typeof firstName, typeof lastName, typeof age);
+// let num = 23567;
+// let numberStr = num.toString();
+// let numArray = numberStr.split("");
+// let result = numArray.join(" ");
+// let number = Number(numArray.join(""));
+
+// console.log(num, numberStr, numArray, result, number);
+
+// console.log(firstName, lastName, age, job);
+// console.log(typeof firstName, typeof lastName, typeof age);
