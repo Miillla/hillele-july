b245b6b834299dace0983dfb2edfc77dfdb03a26
Miillla <p.milana0510@gmail.com>
2024-08-06 14:47:35 +0300

    third commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
-let firstName = "John";
-let lastName = "Doe";
-let age = 35;
-let job = "Web Developer";
+//  3.1 homework 
+
+// let firstName = "John";
+// let lastName = "Doe";
+// let age = 35;
+// let job = "Web Developer";
+
+// console.log(firstName, lastName, age, job);
+
+
+// 3.2 homework
 
 // let firstName = prompt("Enter your first name:");
 // let lastName = prompt("Enter your last name:");
@@ -10,14 +17,45 @@ let job = "Web Developer";
 
 // let fullName = `Fullname: ${firstName} ${lastName} ${age} ${job}`;
 // console.log(fullName);
+// console.log(typeof firstName, typeof lastName, typeof age);
+
+
+
+// 3.3 homework
+
+
+
 
 // let num = 23567;
 // let numberStr = num.toString();
 // let numArray = numberStr.split("");
 // let result = numArray.join(" ");
 // let number = Number(numArray.join(""));
+// let numberStr2 = numberStr.parseInt(numberStr);
 
 // console.log(num, numberStr, numArray, result, number);
 
-console.log(firstName, lastName, age, job);
-console.log(typeof firstName, typeof lastName, typeof age);
+let firstDigit = promt("Enter first digit:");
+console.log(typeof firstDigit);
+ferstDigit = parseInt(firstDigit);
+let result = firstDigit + 100;
+console.log(result);
+
+
+
+
+const currentYear = prompt("Enter current year:");
+const userAge = prompt("Enter your birth year:");
+const yearOfBirth = currentYear - userAge;
+console.log("YOu were born in:" + yearOfBirth);
+
+
+const age = 18
+const isAdult = age >= 18 ? "Adult" : "Not Adult";
+if (age >= 18) {
+    console.log("Adult");
+}else (age < 18) {
+    console.log("Not Adult");
+}
+
+
