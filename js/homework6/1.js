e33dac4ac4b80ecdb65a846335f6d30fa65f84f7
Miillla <p.milana0510@gmail.com>
2024-08-24 11:50:59 +0300

    hw6.1js

--- js/homework6/1.js
+++ js/homework6/1.js
@@ -1,8 +1,26 @@
-// function toRemoveString(str, lettersToRemove) {
-//   let filteredStr = str
-//     .split("")
-//     .filter((char) => !lettersToRemove.includes(char))
-//     .join("");
-//   return filteredStr;
-// }
-// console.log(toRemoveString("Hello world", "ld"));
+function removeLetters(str, lettersToRemove) {
+  let result = [];
+  let arr = str.split("");
+
+  for (let i = 0; i < arr.length; i++) {
+    let letter = arr[i];
+    let match = false;
+
+    for (let ii = 0; ii < lettersToRemove.length; ii++) {
+      if (letter === lettersToRemove[ii]) {
+        console.log("match", match);
+        match = true;
+        break;
+      }
+    }
+
+    if (!match) {
+      result.push(letter);
+    }
+  }
+
+  return result.join("");
+}
+
+let result = removeLetters("Hello world", ["l", "d"]);
+console.log(result);
---
+++ js/homework7/1.js
@@ -0,0 +1,15 @@
+// function sum(x) {
+//   let muns = 4;
+//   return function () {
+//     return nums++;
+//   };
+// }
+
+// const resultFunction = sum(7);
+// console.log(sum(4)); // 4
+
+// // console.log(sum(6)); // 10
+
+// // console.log(sum(10)); // 20
+
+// // console.log(sum(7)); // 27
