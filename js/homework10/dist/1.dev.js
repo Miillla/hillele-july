2c3e6566187ed4c6ceec6f2c36d50ff2cd1fed4b
Miillla <p.milana0510@gmail.com>
2024-09-02 00:51:32 +0300

    hw10.2

--- js/homework10/dist/1.dev.js
+++ js/homework10/dist/1.dev.js
@@ -1,7 +1,11 @@
+// const data = {
+//   name: "Emily",
+//   age: 30,
+//   city: "Paris",
+// };
+// function showData() {
+//   console.log(this);
+//   console.log(`Name: ${this.name}, Age: ${this.age}, City: ${this.city}`);
+// }
+// showData.call(data);
 "use strict";
-
-var data = {
-  name: "Emily",
-  age: 30,
-  city: "France"
-};
