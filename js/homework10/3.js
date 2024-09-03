f55d5058777f3f5fb248be5016fc4da1bc4fe022
Miillla <p.milana0510@gmail.com>
2024-09-03 12:57:52 +0300

    update hw10.3

--- js/homework10/3.js
+++ js/homework10/3.js
@@ -1,5 +1,5 @@
-const data = {
-  friends: [
+const contacts = {
+  data: [
     {
       name: "Kate",
       phoneNums: "+380123456789",
@@ -21,20 +21,61 @@ const data = {
       email: "Vlad@gmail.com",
     },
   ],
-};
-const result = data.friends.find((friend) => friend.name === "Kate");
-console.log(result);
-
-const newFriend = {
-  name: "Anton",
-  phoneNums: "+380999999999",
-  email: "Anton@gmail.com",
+  search: function (name) {
+    return this.data.find((contact) => contact.name === name);
+  },
+  add: function (contact) {
+    this.data.push(contact);
+  },
 };
 
-const updatedArr = data.friends.map((friend) => {
-  return { ...friend, newFriend };
+// Example usage:
+console.log(contacts.search("Kate"));
+
+contacts.add({
+  name: "John",
+  phoneNums: "+380222222222",
+  email: "John@gmail.com",
 });
-console.log(updatedArr);
+console.log(contacts.data);
 
-data.friends.push(newFriend);
-console.log(data.friends);
+// const data = {
+//   friends: [
+//     {
+//       name: "Kate",
+//       phoneNums: "+380123456789",
+//       email: "Kate@gmail.com",
+//     },
+//     {
+//       name: "Kolya",
+//       phoneNums: "+380987654321",
+//       email: "Kolya@gmail.com",
+//     },
+//     {
+//       name: "Helen",
+//       phoneNums: "+380111111111",
+//       email: "Helen@gmail.com",
+//     },
+//     {
+//       name: "Vlad",
+//       phoneNums: "+380111111117",
+//       email: "Vlad@gmail.com",
+//     },
+//   ],
+// };
+// const result = data.friends.find((friend) => friend.name === "Kate");
+// console.log(result);
+
+// const newFriend = {
+//   name: "Anton",
+//   phoneNums: "+380999999999",
+//   email: "Anton@gmail.com",
+// };
+
+// const updatedArr = data.friends.map((friend) => {
+//   return { ...friend, newFriend };
+// });
+// console.log(updatedArr);
+
+// data.friends.push(newFriend);
+// console.log(data.friends);
