68cdeee6af4a4417d65246b3400f614ac7e8daef
Miillla <p.milana0510@gmail.com>
2024-09-07 17:39:15 +0300

    hw12.2

--- js/homework12/2.js
+++ js/homework12/2.js
@@ -1,10 +1,6 @@
-// const parents = document.querySelector("#parents");
-// parents.addEventListener("click", (event) => {
-//   const target = event.target;
-//   if (
-//     target.tagName === "BUTTON" &&
-//     event.target.getAttribute("data-action") === "click"
-//   ) {
-//     event.target.parentNode.click();
-//   }
-// });
+const parents = document.querySelector("#parents");
+parents.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    alert(`Ви клікнули на: ${event.target.textContent}`);
+  }
+});
--- js/homework12/dist/2.dev.js
+++ js/homework12/dist/2.dev.js
@@ -1,11 +1,8 @@
-// const parents = document.querySelector("#parents");
-// parents.addEventListener("click", (event) => {
-//   const target = event.target;
-//   if (
-//     target.tagName === "BUTTON" &&
-//     event.target.getAttribute("data-action") === "click"
-//   ) {
-//     event.target.parentNode.click();
-//   }
-// });
 "use strict";
+
+var parents = document.querySelector("#parents");
+parents.addEventListener("click", function (event) {
+  if (event.target.tagName === "BUTTON") {
+    alert("\u0412\u0438 \u043A\u043B\u0456\u043A\u043D\u0443\u043B\u0438 \u043D\u0430: ".concat(event.target.textContent));
+  }
+});
