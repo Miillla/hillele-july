873d14c84126e33b1cc22cac4f541b5767ebc0a9
Miillla <p.milana0510@gmail.com>
2024-09-23 14:14:23 +0300

    hw14.1

--- dist/zzzzz.dev.js
+++ dist/zzzzz.dev.js
@@ -2,31 +2,40 @@
 
 var leftArrow = document.querySelector(".left");
 var rightArrow = document.querySelector(".right");
-var track = document.querySelector(".slide-container");
+var track = document.querySelector(".slide - container");
 var cardWrapper = document.querySelector(".card-wrapper");
 var slides = Array.from(cardWrapper.children);
-var dotsNav = document.querySelector(".carousel__nav");
+var dotsNav = document.querySelectorAll(".carousel__nav");
 var dots = Array.from(dotsNav.children);
-var slideWidth = slides[0].getBoundingClientRect().width;
+var slideWidth = slides[0].getBoundingClientsRect().width;
+
+var setSlidePosition = function setSlidePosition(slide, index) {
+  slide.style.left = slideWidth * index + "px";
+};
+
+slides.forEach(setSlidePosition);
 
 var moveToSlide = function moveToSlide(track, currentSlide, targetSlide) {
+  // track.style.transform = "translateX('+ targetSlide.style.left +')";
   track.style.transform = "translateX(-" + targetSlide.offsetLeft + "px)";
   currentSlide.classList.remove("current-slide");
   targetSlide.classList.add("current-slide");
 };
 
 rightArrow.addEventListener("click", function (e) {
-  var currentSlide = cardWrapper.querySelector(".current-slide");
+  var currentSlide = cardWrapper.querySelector(". current-slide"); // const prevSlide = currentSlide.previousElementSibling;
+
   var nextSlide = currentSlide.nextElementSibling;
-  moveToSlide(cardWrapper, currentSlide, nextSlide);
+  moveToSlide(track, currentSlide, nextSlide);
 });
 leftArrow.addEventListener("click", function (e) {
-  var currentSlide = cardWrapper.querySelector(".current-slide");
+  var currentSlide = track.querySelector(". current-slide");
   var prevSlide = currentSlide.previousElementSibling;
-  moveToSlide(cardWrapper, currentSlide, prevSlide);
+  moveToSlide(track, currentSlide, prevSlide);
 });
 dotsNav.addEventListener("click", function (e) {
-  var targetDot = e.target.closest("li");
+  // const targetDot = e.taget.closest("button");
+  var targetDot = e.target;
   if (!targetDot) return;
   var currentSlide = track.querySelector(".current-slide");
   var currentDot = dotsNav.querySelector(".current-slide");
--- js/homework14/1.js
+++ js/homework14/1.js
@@ -1,39 +1,33 @@
 const leftArrow = document.querySelector(".left");
 const rightArrow = document.querySelector(".right");
-const track = document.querySelector(".slide - container");
-const slides = Array.from(track.children);
+const track = document.querySelector(".slide-container");
 const cardWrapper = document.querySelector(".card-wrapper");
-const dotsNav = document.querySelectorAll(".carousel__nav");
+const slides = Array.from(cardWrapper.children);
+const dotsNav = document.querySelector(".carousel__nav");
 const dots = Array.from(dotsNav.children);
 
-const slideWidth = slides[0].getBoundingClientsRect().width;
-
-const setSlidePosition = (slide, index) => {
-  slide.style.left = slideWidth * index + "px";
-};
-
-slides.forEach(setSlidePosition);
+const slideWidth = slides[0].getBoundingClientRect().width;
 
 const moveToSlide = (track, currentSlide, targetSlide) => {
-  track.style.transform = "translateX('+ targetSlide.style.left +')";
+  track.style.transform = "translateX(-" + targetSlide.offsetLeft + "px)";
   currentSlide.classList.remove("current-slide");
   targetSlide.classList.add("current-slide");
 };
 
 rightArrow.addEventListener("click", (e) => {
-  const currentSlide = track.querySelector(". current-slide");
-  const prevSlide = currentSlide.previousElementSibling;
-
-  moveToSlide(track, currentSlide, prevSlide);
+  const currentSlide = cardWrapper.querySelector(".current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+  moveToSlide(cardWrapper, currentSlide, nextSlide);
 });
 
 leftArrow.addEventListener("click", (e) => {
-  const currentSlide = track.querySelector(". current-slide");
-  const nextSlide = currentSlide.nextElementSibling;
-  moveToSlide(track, currentSlide, nextSlide);
+  const currentSlide = cardWrapper.querySelector(".current-slide");
+  const prevSlide = currentSlide.previousElementSibling;
+  moveToSlide(cardWrapper, currentSlide, prevSlide);
 });
+
 dotsNav.addEventListener("click", (e) => {
-  const targetDot = e.taget.closest("button");
+  const targetDot = e.target.closest("li");
   if (!targetDot) return;
 
   const currentSlide = track.querySelector(".current-slide");
